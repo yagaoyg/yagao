246c0fdb9fe77f4ebfbe13df97cc27aafae538df
yagao <949899039@qq.com>
2025-02-22 18:38:02 +0800

    使用vite插件gzip压缩

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression'
+import { visualizer } from "rollup-plugin-visualizer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    viteCompression(),
+    visualizer({ open: true })
   ],
   resolve: {
     alias: {
